ce1730579f6c5e7ab57a21d2adab4e5e6642d242
Alejandro Dominguez <adborroto90@gmail.com>
2024-10-04 11:52:15 +0200

    fix: remove private packages deps
    Signed-off-by: Alejandro Dominguez <adborroto90@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
-const jestCoverageConfig = require('@mixmaxhq/jest-coverage-config/typescript');
 const jestJunitConfig = process.env.CI && require('@mixmaxhq/jest-junit-config');
+
+const jestCoverageConfig = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testRegex: '/((test|spec)s?|src)/.*([Tt]est|[Ss]pec)\\.(ts|js)$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+  preset: 'ts-jest',
+};
 
 module.exports = {
   clearMocks: true,
